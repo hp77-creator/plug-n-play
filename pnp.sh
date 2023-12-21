4a7795301d478786b46d1a1142e3c665b5015d31
Himanshu Pandey <himanshu.pandey@piramal.com>
2023-12-21 16:48:44 +0530

    hp77 | Add bash version for pnp

---
+++ pnp.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+
+if [ $# -eq 0 ]; then
+	echo "Usage: $0 <url>"
+	exit 1
+fi
+
+url="$1"
+echo "URL: $url"
+curl -# -O "$url"
+latest_file=$(ls -t | head -n 1)
+destination="/Users/himanshu.pandey/.plug_n_play/"
+mv "$latest_file" "$destination"
+open "$destination$latest_file"
